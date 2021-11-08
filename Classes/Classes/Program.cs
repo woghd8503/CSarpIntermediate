e2b950f486e2572c3b8088565d6283fd4c67b4b0
James <woghd8503@naver.com>
2021-11-08 12:00:00 +0900

    Classes

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var Person = new Person();
+            Person.Name = str;
+
+            return Person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
         }
     }
 }
